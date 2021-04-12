6c6694f70f6d17eac27f97f475dd25cb1d592303
Taiizor <Taiizor@github.com>
2021-04-12 22:43:00 +0300

    53X Again 1!

---
+++ src/Witcher/Witcher.cs
@@ -0,0 +1,34 @@
+﻿#region Imports
+
+using System;
+
+#endregion
+
+// |---------DO-NOT-REMOVE---------|
+//
+//     Creator: Taiizor
+//     Website: www.Soferity.com
+//     Created: 12.Apr.2021
+//     Changed: 12.Apr.2021
+//     Version: 1.0.0.1
+//
+// |---------DO-NOT-REMOVE---------|
+
+namespace Witcher
+{
+    #region Core
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Witcher
+    {
+        #region Name
+
+        ///
+
+        #endregion
+    }
+
+    #endregion
+}
