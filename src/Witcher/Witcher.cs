870f7010e4b23531ffda1eaf72705c0245615650
Taiizor <taiizor@soferity.com>
2021-12-25 23:47:45 +0300

    53X Again 54!

--- src/Witcher/Witcher.cs
+++ src/Witcher/Witcher.cs
@@ -9,7 +9,7 @@ using Witcher.Value;
 //     Creator: Taiizor
 //     Website: www.Soferity.com
 //     Created: 12.Apr.2021
-//     Changed: 22.Dec.2021
+//     Changed: 25.Dec.2021
 //     Version: 1.0.0.3
 //
 // |---------DO-NOT-REMOVE---------|
